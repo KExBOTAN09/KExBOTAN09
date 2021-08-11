0f5f014dcbdaffd65f6dc00f859f721aa508fc71
KExBOTAN09 <88756516+KExBOTAN09@users.noreply.github.com>
2021-08-11 06:17:34 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true})
+
+
+client.on('message', message => { 
+
+if (!message.guild) return; 
+
+if (message.content === 'wara') { 
+ if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+if (message.member.voiceChannel) { 
+
+message.member.voiceChannel.join() 
+
+.then(connection => { 
+
+
+}) 
+
+.catch(console.log); 
+
+} else { 
+
+message.reply('**<a:9FAF7C0CF0104A2F979F1263DF55D91A:855230044258828328> پێویستە تۆ لە ڤۆیسەکە بیت **'); 
+
+} 
+
+} 
+
+});
+
+
+client.on("message" , message => {
+
+if(message.channel.type === 'dm') {
+
+if (message.content.startsWith("https://discord.gg/")) {
+    
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/JSU8tGcXdD`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+    
+  client.channels.get("863805059019178014").send(
+`>  By: <@${message.author.id}> ✅ 
+${message.content}`)
+}
+    }
+        })
+
+
+
+
+client.on('ready', () => {
+console.log(`ON ${client.guilds.size} Servers`);
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Botan 💙`) 
+});
+
+
+
+
+client.login("ODY4NTQ4MjU0NTc1NTAxMzMy.YPxQmA.cA-pI2kEiVskLzTiaLMQ_NoQr1w");// Your Token
